f3c010bce47647f5e42397a64f56e6cd1410fbd8
Sanved <sanved77@gmail.com>
2019-02-25 21:48:51 -0500

    .gitignore fix

---
+++ Assets/CamFollow.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamFollow : MonoBehaviour{
+    // Start is called before the first frame update
+    
+    [SerializeField]
+    Transform player;
+
+    Vector3 offset;
+
+    [SerializeField]
+    float smoothRate;
+
+    void Start()
+    {
+        offset = player.position - transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate(){
+
+        Vector3 currPos = transform.position;
+        Vector3 newPos = player.position - offset;
+        
+        transform.position = Vector3.Lerp(currPos, newPos, smoothRate);
+
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour{
+
+    Rigidbody rb;
+    Animator anim;
+
+    Transform player;
+    
+    int frameFlicker = 0;
+
+    float zero = 0;
+
+    [SerializeField]
+    float speed;
+
+    [SerializeField]
+    float jumpForce;
+    
+
+    void Awake(){
+        // init call
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+        player = GetComponent<Transform>();
+    }
+    // Start is called before the first frame update
+    void Start(){
+        //rb.velocity = Vector3.forward * speed;
+    }
+
+    // Update is called once per frame
+    void Update(){
+        
+        //change to screen touch
+
+        if(frameFlicker == 30){
+            rb.velocity = Vector3.forward * speed;
+            frameFlicker = 0;
+        }else{
+            frameFlicker++;
+        }
+
+        if(Input.GetMouseButtonDown(0)){
+            anim.SetTrigger("jump");
+            //rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            turnLeft();
+        }
+
+    }
+
+    void turnLeft(){
+
+        player.Rotate(Vector3.up, -90);
+        rb.velocity = Vector3.forward * 0;
+        
+
+    }
+
+    void turnRight(){
+
+        player.Rotate(Vector3.up, 90);
+
+    }
+}
